a4b421dc206ffff5233260b4cda9a4ba8975e08f
Ivan <ivangopnik707@gmail.com>
2026-04-27 00:44:20 +0300

    Закомментирован main

--- source/main.cpp
+++ source/main.cpp
@@ -4,10 +4,13 @@
 
 int main(int argc, char* argv[])
 {
+    //Создаем приложение.
     QApplication app{ argc, argv };
 
+    //Создаем объект окна приложения.
     MainWindow mainWindow{};
     mainWindow.show();
 
+    //Запускаем приложения.
     return app.exec();
 }
